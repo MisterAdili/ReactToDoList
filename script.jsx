6cb42d947fc65025dd083b0d2acc51132fae031c
Jacob Adili <63510880+MisterAdili@users.noreply.github.com>
2024-12-30 10:14:51 -0600

    Add files via upload

--- script.jsx
+++ script.jsx
@@ -1,6 +1,5 @@
 const checkStatus = (response) => {
   if (response.ok) {
-
     return response;
   }
   throw new Error('Request was either a 404 or 500');
@@ -8,108 +7,186 @@ const checkStatus = (response) => {
 
 const json = (response) => response.json()
 
-const Movie = (props) => {
-  const { 
-    Title, 
-    Year, 
-    imdbID, 
-    Type, 
-    Poster, 
-  } = props.movie;
+class Task extends React.Component {
+  render () {
+    const { task, onDelete, onComplete } = this.props;
+    const { id, content, completed } = task;
 
-  return (
-    <div className="row">
-      <div className="col-4 col-md-3 mb-3">
-        <a href={`https://www.imbd.com/title/${imdbID}/`} target="_blank">
-          <img src={Poster} className="img-fluid" />
-        </a>
-      </div> 
-      <div className="col-8 col-md-3 mb-3">
-        <a href={`https://www.imdb.com/title/${imdbID}/`} target="_blank">
-          <h4>{Title}</h4>
-          <p>{Type} | {Year}</p>
-        </a>
+    return(
+      <div className="row mb-1">
+        <p className="col">{content}</p>
+        <button onClick={() => onDelete(id)}>Delete</button>
+        <input className="d-inline-block-mt-2" type="checkbox" onChange={() => onComplete(id, completed)} checked={completed} />
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-class MovieFinder extends React.Component {
+class ToDoList extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      searchTerm: '',
-      results: [],
-      error: '',
+      new_task: '',
+      tasks: [],
+      filter: 'all'
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.fetchTasks = this.fetchTasks.bind(this);
+    this.deleteTask = this.deleteTask.bind(this);
+    this.toggleComplete = this.toggleComplete.bind(this);
+    this.toggleFilter = this.toggleFilter.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchTasks();
+  }
+
+  fetchTasks() {
+    fetch("https://fewd-todolist-api.onrender.com/tasks?api_key=1254")
+    .then(checkStatus)
+    .then(json)
+    .then((response) => {
+      console.log(response);
+      this.setState({tasks: response.tasks});
+    })
+    .catch(error => {
+      console.error(error.message);
+    })
   }
 
   handleChange(event) {
-    this.setState({ searchTerm: event.target.value });
+    this.setState({ new_task: event.target.value });
   }
 
   handleSubmit(event){
     event.preventDefault();
-    let { searchTerm } = this.state;
-    searchTerm = searchTerm.trim();
-    if (!searchTerm) {
+    let { new_task } = this.state;
+    new_task = new_task.trim();
+    if (!new_task) {
       return;
     }
 
-    fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=b7da8d63`)
-      .then(checkStatus)
-      .then(json)
-      .then((data) => {
-        if (data.Response === 'False') {
-          throw new Error(data.Error);
+    fetch("https://fewd-todolist-api.onrender.com/tasks?api_key=1254", {
+      method: "POST",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        task: {
+          content: new_task
         }
-
-        if (data.Response === 'True' && data.Search) {
-          this.setState({ results: data.Search, error: '' });
-        }
-      })
-      .catch((error) => {
-        this.setState({ error: error.message });
-        console.log(error);
-      })
+      }),
+    }).then(checkStatus)
+    .then(json)
+    .then((data) => {
+      this.setState({new_task: ''});
+      this.fetchTasks();
+    })
+    .catch((error) => {
+      this.setState({error: error.message});
+      console.log(error);
+    })
   }
 
-  render() {
-    const { searchTerm, results, error } = this.state;
+  deleteTask(id) {
+    if (!id) {
+      return;
+    }
+
+    fetch("https://fewd-todolist-api.onrender.com/tasks/${id}?api_key=1254", {
+      method: "DELETE",
+      mode: "cors",
+    }).then(checkStatus)
+    .then(json)
+    .then((data) => {
+      this.fetchTasks();
+    })
+    .catch((error) => {
+      this.setState({error:error.message});
+      console.log(error);
+    })
+  }
+
+  toggleComplete(id, completed) {
+    if (!id) {
+      return;
+    }
+    const newState = completed ? 'active' : 'complete';
+    fetch("https://fewd-todolist-api.onrender.com/tasks/${id}/mark_${newState}?api_key=1254", {
+      method: "PUT",
+      mode: "cors",
+    }).then(checkStatus)
+    .then(json)
+    .then((data) => {
+      this.fetchTasks();
+    })
+    .catch((error) => {
+      this.setState({error:error.message});
+      console.log(error);
+    })
+  }
+
+  toggleFilter(e) {
+    console.log(e.target.name)
+    this.setState({filter: e.target.name})
+  }
+
+  render(){
+    const {new_task, tasks, filter} = this.state;
 
     return (
       <div className="container">
         <div className="row">
           <div className="col-12">
+            <h2 className="mb-3">To Do List</h2>
+            {tasks.length > 0 ? tasks.filter(task => {
+              if (filter === 'all'){
+                return true;
+              } else if (filter === 'active') {
+                return !task.completed;
+              } else {
+                return task.completed;
+              }
+            }).map((task) => {
+              return (<Task
+                key={task.id}
+                task={task}
+                onDelete={this.deleteTask}
+                onComplete={this.toggleComplete}
+                />
+              );
+            }) : <p>no tasks here</p>}
+            
+            <div className="mt-3">
+              <label>
+                <input type="checkbox" name="all" checked={filter === "all"} onChange={this.toggleFilter} /> all
+              </label>
+              <label>
+                <input type="checkbox" name="active" checked={filter === "active"} onChange={this.toggleFilter} /> active
+              </label>
+              <label>
+                <input type="checkbox" name="completed" checked={filter === "completed"} onChange={this.toggleFilter} /> completed
+              </label>
+            </div>
+
             <form onSubmit={this.handleSubmit} className="form-inline my-4">
               <input
                 type="text"
                 className="form-control mr-sm-2"
-                placeholder="frozen"
-                value={searchTerm}
+                placeholder="new task"
+                value={new_task}
                 onChange={this.handleChange}
-                />
-                <button type="submit" className="btn btn-primary">Submit</button>
+              />
+              <button type="submit" className="btn btn-primary">Submit</button>
             </form>
-            {(() => {
-              if(error) {
-                return error;
-              }
-              return results.map((movie) => {
-                return <Movie key={movie.imdbID} movie={movie} />;
-              })
-            })()}
           </div>
         </div>
       </div>
     )
   }
-
 }
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<MovieFinder />);
+root.render(<ToDoList />);
